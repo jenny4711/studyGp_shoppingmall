187d157477382aa0389b6a9aaaa441e8c57114fe
jiylee <jjenny4711@gmail.com>
2024-03-28 00:06:54 -0400

    search

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 import { Person ,Search} from 'react-bootstrap-icons';
 import { useNavigate ,Link} from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import './NavBar.css'
 import { menuList } from '../utils/menus';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import Login from '../pages/Login';
 const NavBar = ({logIn,setLogIn}) => {
   const navigation=useNavigate()
+  const [search,setSearch]=useState("")
+  const [query,setQuery]=useSearchParams();
+
+useEffect(()=>{
+  if(search){
+    setQuery({q:search})
+  }else{
+    setQuery()
+  }
+
+},[search])
+
+
   const handleLogOut=(evt)=>{
     evt.preventDefault();
     if(evt.target.text === '로그아웃'){
       setLogIn(false)
-      navigation('/login')
+      navigation('/')
     }else{
      
       navigation('/login')
@@ -33,7 +54,7 @@ const NavBar = ({logIn,setLogIn}) => {
        <Link to='/'><img width={200}  src='https://www.hm.com/entrance/assets/bundle/img/HM-Share-Image.jpg'/></Link> 
       </div>
 {/* ------------------------------------------------------- */}
-      <div className={!logIn?'none':'menuArea'}>
+      {/* <div className={!logIn?'none':'menuArea'}>
         
           <ul className='menuList'>
           {menuList.map(item=>(<li>{item}</li>))}
@@ -41,9 +62,45 @@ const NavBar = ({logIn,setLogIn}) => {
         
         <div className='searchSec'>
           <Search size={20}/>
-          <input className='input' type='text'/>
+          <input  className='input' type='text' onChange={(evt)=>setSearch(evt.target.value)} placeholder='Search'/>
         </div>
-      </div>
+      </div> */}
+
+<Navbar key={"md"} expand={"md"} className="bg-body-tertiary mb-3 menuList" style={{"backgroundColor":"white"}}>
+          <Container  fluid>
+           
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"md"}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${"md"}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${"md"}`}
+              placement="end"
+            >
+             
+              <Offcanvas.Body>
+                <Nav className="menuList">
+                {menuList.map(item=>(<Nav.Link>{item}</Nav.Link>))}
+                 
+               
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2 searchInput"
+                    aria-label="Search"
+                    onChange={(evt)=>setSearch(evt.target.value)}
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+
+
+
+
+
      {/* ---------------------------------------------------------- */}
     </div>
   );
--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -2,14 +2,31 @@ import React ,{useState,useEffect}from 'react';
 import { useAllProductQuery } from '../hooks/useGetProduct';
 import ProductCard from '../components/ProductCard';
 import { Container, Row ,Col} from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 import './ProductAll.css'
 const ProductAll = () => {
   const [productList,setProductList]=useState([])
+  const [query,setQuery]=useSearchParams();
   const {data}=useAllProductQuery()
-  console.log(data?.data)
+  let searchQuery = query.get("q") || []
+
   useEffect(()=>{
-setProductList(data?.data)
-  },[data])
+
+let lists=data?.data
+if(lists && searchQuery){
+  let filterLists = lists.filter((item)=>{
+    let name = item.name.toLowerCase()
+    return name.includes(searchQuery);
+  })
+ 
+  setProductList(filterLists)
+}else{
+  setProductList(data?.data)
+}
+
+
+  },[searchQuery,data])
+
   return (
     <div className='ProductAll'>
       <Container className='ProductAll_Container'>
